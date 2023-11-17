3a7f74342b545fae30dfaf74451908b32414c33e
Phagos-pop <zaroxsino@gmail.com>
2023-11-17 14:20:13 +0300

    Unity version switch

--- Assets/Scripts/CatWalk/CatWalkPoint.cs
+++ Assets/Scripts/CatWalk/CatWalkPoint.cs
@@ -6,12 +6,6 @@ namespace CatWalk
 {
     public class CatWalkPoint : MonoBehaviour
     {
-        private Transform _transform;
-        public Vector3 Position { get { return _transform.position; }}
-
-        private void Start()
-        {
-            _transform = gameObject.transform;
-        }
+        public Vector3 Position { get { return transform.position; }}
     }
 }
